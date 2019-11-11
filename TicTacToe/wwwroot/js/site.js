a183b27eee71127a1bc61a9b61727c27c6b77033
Dustin Wilson <dustin52@users.campus>
2019-11-11 17:13:03 -0600

    Completed daily work

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,59 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var currentTurn = "X";
+
+document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+
+var squares = document.getElementsByClassName("square");
+
+for (var i = 0; i < squares.length; i++)
+{
+    squares[i].addEventListener('click', onClick);
+}
+
+function onClick(event)
+{
+    event.preventDefault();
+
+    if (!event.target.innerText) {
+        event.target.innerHTML = currentTurn;
+        nextTurn();
+        checkForWin();
+    }
+
+}
+
+function nextTurn()
+{
+    if (currentTurn === 'X') 
+        currentTurn = 'O';
+    else
+        currentTurn = 'X';
+
+    document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+}
+
+function checkForWin()
+{
+    // Check Rows
+    for (i = 0; i < squares.length; i = i + 3) {
+        if (squares[i].innerText === squares[i + 1] && squares[i].innerText === squares[i + 2] && squares[i].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+
+    //Check Columns
+    for (j = 0; j < squares.length; j++) {
+        if (squares[j].innerText === squares[j + 3] && squares[j].innerText === squares[j + 6] && squares[i].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+}
+
+function declareWinner{
+    document.getElementById("turn").innerText = "Player " + currentTurn + " wins!";
+}

71bb25be5c27260967da99fd64b49855df797552
Dustin Wilson <dustin52@users.campus>
2019-11-15 17:06:31 -0600

    Checkers

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -58,3 +58,34 @@ function checkForWin()
 function declareWinner{
     document.getElementById("turn").innerText = "Player " + currentTurn + " wins!";
 }
+
+var squares = document.getElementsByClassName("square");
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('dragenter', onDragEnter);
+    squares[i].addEventListener('dragleave', onDragLeave);
+    squares[i].addEventListener('dragstart', onDragStart);
+    squares[i].addEventListener('dragend', onDragEnd);
+}
+
+function onDragEnter(event) {
+    if (event.target.classList.contains("checker")) return;
+    if (event.target.classList.contains("red")) return;
+    if (event.target.children.length > 0) return;
+    event.target.style.backgroundColor = "gold";
+    document.getElementById("toX").value = event.target.dataset.x;
+    document.getElementById("toY").value = event.target.dataset.y;
+}
+
+function onDragLeave(event) {
+    event.target.style.backgroundColor = null;
+}
+
+function onDragStart(event) {
+    document.getElementById("fromX").value = event.target.dataset.x;
+    document.getElementById("fromY").value = event.target.dataset.y;
+}
+
+function onDragEnd(event) {
+    document.getElementById("checkers-form").submit();
+}
